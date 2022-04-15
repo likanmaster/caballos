bd877d1262debb403f5bf73c8f708495e62e6189
jose <jlicancura@ing.ucsc.cl>
2022-04-15 12:28:45 -0400

    asd

--- src/caballos/Caballos.java
+++ src/caballos/Caballos.java
@@ -5,21 +5,21 @@ package caballos;
  * @author pc0116
  */
 public class Caballos {
-
+     
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
        
-        int c=5;
-        int m=50;
+        int c=2;
+        int m=60;
         int hipodromo[][] = new int[c][m];
         int posicion[]=new int[c];
         boolean flag=false;//para saber si un caballo pasó o llego a la meta
        
         int  meta=crearmeta(m); //creamos la meta
         System.out.println("meta "+meta);//mostramos donde estará la meta
-        //iniciar caballos en posicion 0
+        //iniciar la posicion caballos en posicion 0
         for (int i = 0; i < posicion.length; i++) {
             posicion[i]=0;
         }
@@ -29,26 +29,31 @@ public class Caballos {
              for (int j = 0; j < m; j++) {
                 if (j!=meta) {//verificamos que no sea la posicion de meta
                     hipodromo[i][j]=0;    
-                }else hipodromo[i][j]=8;//asignamos la meta como un 8
+                }else hipodromo[i][j]=1;//asignamos la meta como un 1
             }
             hipodromo[i][0]=1;//iniciMOs los caballos en la partida
         }
         //mostrar matriz y caballos
-        System.out.println("hipodromo con pocisiones iniciales");
-        System.out.println("1=Caballo  0=pocicion vacia  8=Meta ");
+        //System.out.println("hipodromo con pocisiones iniciales");
+        System.out.println("☻=Caballo  0=pocicion vacia  1=Meta ");
         mostrarmatriz(hipodromo);//mostramos el hipodromo inicial
        
        
        //inicia partida   carrera
 
     while(flag==false){//vamos comprobando si hay ganadores
+        System.out.println("Corriendo... ");
+        Thread.sleep(2000);
+        dibujarmatriz(posicion,hipodromo);
         for (int i = 0; i < c; i++) {
-            if (posicion[i]>=meta) {
+            if (posicion[i]>meta) {
                  System.out.println(" primer caballo en llegar a la meta "+i);
                  flag=true;
             }
             else  posicion=modpos(posicion, meta);
         }
+    
+        
     }
      
     
@@ -58,12 +63,10 @@ public class Caballos {
        //    System.out.println(" "+posicion[i]);
       //  }
     //dibujamos matriz final
-    
+     System.out.println("resultado final  ");
     dibujarmatriz(posicion,hipodromo);
-    }
-   
-   
-   
+
+       }
 //funciones
 
     private static int crearmeta(int m) {
@@ -104,10 +107,11 @@ public class Caballos {
             for (int j = 0; j < hipodromo[0].length; j++) {
                 hipodromo[i][0]=0;//con esto limpiamos la matriz original  
                 if (posicion[i]==j) {
-                     System.out.print("1 ");
+                     System.out.print("☻ ");
                 }else  System.out.print(hipodromo[i][j]+" ");  
             }
              System.out.println(" ");
         }       
     }
+    
 }
